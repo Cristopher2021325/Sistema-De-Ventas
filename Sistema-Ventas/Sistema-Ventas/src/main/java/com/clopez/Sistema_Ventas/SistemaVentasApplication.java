fbf119b3612ce586a8dfe0aeb17b9f9273513bdc
Cristopher <clopez-2021325@kinal.edu.gt>
2026-04-15 18:55:23 -0600

    feat: se subio el proyecto sistema ventas

---
+++ Sistema-Ventas/Sistema-Ventas/src/main/java/com/clopez/Sistema_Ventas/SistemaVentasApplication.java
@@ -0,0 +1,13 @@
+package com.clopez.Sistema_Ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVentasApplication.class, args);
+	}
+
+}
